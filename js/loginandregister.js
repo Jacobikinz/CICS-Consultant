4be8e169354055bbdaa93d48f455a84431b649c6
Thomas Callaghan <callaghantw@gmail.com>
2022-03-25 15:23:04 -0400

    everything refactored and formatted

--- js/loginandregister.js
+++ js/loginandregister.js
@@ -1,13 +1,12 @@
 const signinbutton = document.getElementById('signinbutton');
 console.log(signinbutton);
 
-signinbutton.addEventListener("click", function(e) {
+signinbutton.addEventListener("click", function (e) {
     document.location.href = 'signin.html';
-    console.log('hi');
+    // console.log('hi');
 });
 
 const registerbutton = document.getElementById('registerbutton');
-registerbutton.addEventListener("click", function(e) {
+registerbutton.addEventListener("click", function (e) {
     document.location.href = 'register.html';
-    
 });
