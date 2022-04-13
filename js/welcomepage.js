24bbae8506472abd75ad88d3a4da5c26e58c9ff7
hawuter <thehannahwu@gmail.com>
2022-04-12 21:31:18 -0400

    Add EventHandler for all dd

--- js/welcomepage.js
+++ js/welcomepage.js
@@ -1,23 +1,27 @@
 import { Quiz } from './quiz.js';
 let quiz = new Quiz("email");
 
-let x = document.getElementById('CS-courses-dd');
+const questions = [];
+questions.push(document.getElementById('CS-courses-dd'));
+questions.push(document.getElementById('math-courses-dd'));
+questions.push(document.getElementById('science-courses-dd'));
 
-console.log(x);
 
-function updateQuiz(e) {
+function ddUpdateQuiz(e, i) {
     let selectedvals = [];
-    for (let i = 0; i < x.length; i++) {
-        let curroption = x.options[i];
+    for (let i = 0; i < e.length; i++) {
+        let curroption = e.options[i];
         if (curroption.selected === true) {
             selectedvals.push(curroption.label);
         }
     }
     console.log(selectedvals);
     const json = quiz.json;
-    json.questions[0].selected = selectedvals;
+    json.questions[i].selected = selectedvals;
     quiz.answers = json;
     console.log(quiz.json);
 }
 
-x.addEventListener("change", updateQuiz);
+for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("change", e => ddUpdateQuiz(questions[i], i));
+}
