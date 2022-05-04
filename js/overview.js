639d8fc9fc823cf5e8b085601d7d032542d077f2
Jacob Urisman <jacobu2001@yahoo.com>
2022-05-04 01:32:35 -0400

    accordions

--- js/overview.js
+++ js/overview.js
@@ -6,13 +6,37 @@ const classesData = await classes.json();
 function fillClasses(str) {
   const table = document.getElementById(str);
   const field = fieldsData.find(v => v.field === str);
+  let counter = 1;
   for (let id of field.classes){
-    const item = document.createElement('li');
-    item.classList.add('list-group-item');
-    item.appendChild(document.createTextNode(`${id} - ${classesData.find(v => v.id === id).name}`));
+    const course = classesData.find(v => v.id === id);
+    const item = document.createElement('div');
+    item.classList.add('accordion-item');
+    const header = document.createElement('h2');
+    header.classList.add('accordion-header');
+    header.id = `${str}${course.id}head`;
+    const button = document.createElement('button');
+    button.setAttribute("type", "button");
+    button.classList.add('accordion-button');
+    button.classList.add('collapsed');
+    button.setAttribute("data-bs-toggle", "collapse");
+    button.setAttribute("data-bs-target", `#${str}${course.id}collapse`);
+    button.appendChild(document.createTextNode(`${id} - ${course.name}`));
+    header.appendChild(button);
+    item.appendChild(header);
+    const content = document.createElement('div');
+    content.classList.add('accordion-collapse');
+    content.classList.add('collapse');
+    content.id = `${str}${course.id}collapse`;
+    const body = document.createElement('div');
+    body.classList.add('accordion-body');
+    body.appendChild(document.createTextNode(course.description));
+    content.appendChild(body);
+    item.appendChild(content);
     table.appendChild(item);
   }
 }
+
+// <button data-bs-target="#collapseOne">
 
 fillClasses('ai');
 fillClasses('data');
